3682ba723c43e705e91f613851d4323c2874aa5b
KadenThomp36 <kadenthomp36@gmail.com>
2024-08-28 14:35:29 -0400

    4.1.0

--- src/DreoAPI.ts
+++ src/DreoAPI.ts
@@ -3,32 +3,39 @@ import MD5 from 'crypto-js/md5';
 import ReconnectingWebSocket from 'reconnecting-websocket';
 import WebSocket from 'ws';
 
+const ua = 'dreo/2.8.1 (iPhone; iOS 17.6.1; Scale/3.00)';
+
 // Follows same request structure as the mobile app
 export default class DreoAPI {
   // Get authentication token
   public async authenticate(platform, email, password, server) {
     let token;
-    await axios.post('https://app-api-'+server+'.dreo-cloud.com/api/oauth/login', {
-      'client_id': '7de37c362ee54dcf9c4561812309347a',
-      'client_secret': '32dfa0764f25451d99f94e1693498791',
-      'email': email,
-      'encrypt': 'ciphertext',
-      'grant_type': 'email-password',
-      'himei': 'faede31549d649f58864093158787ec9',
-      'password': MD5(password).toString(),  // MD5 hash is sent instead of actual password
-      'scope': 'all',
-    }, {
-      params: {
-        'timestamp': Date.now(),
-      },
-      headers: {
-        'ua': 'dreo/2.5.12 (sdk_gphone64_arm64;android 13;Scale/2.625)',
-        'lang': 'en',
-        'content-type': 'application/json; charset=UTF-8',
-        'accept-encoding': 'gzip',
-        'user-agent': 'okhttp/4.9.1',
-      },
-    })
+    await axios
+      .post(
+        'https://app-api-' + server + '.dreo-cloud.com/api/oauth/login',
+        {
+          client_id: '7de37c362ee54dcf9c4561812309347a',
+          client_secret: '32dfa0764f25451d99f94e1693498791',
+          email: email,
+          encrypt: 'ciphertext',
+          grant_type: 'email-password',
+          himei: 'faede31549d649f58864093158787ec9',
+          password: MD5(password).toString(), // MD5 hash is sent instead of actual password
+          scope: 'all',
+        },
+        {
+          params: {
+            timestamp: Date.now(),
+          },
+          headers: {
+            ua: ua,
+            lang: 'en',
+            'content-type': 'application/json; charset=UTF-8',
+            'accept-encoding': 'gzip',
+            'user-agent': 'okhttp/4.9.1',
+          },
+        },
+      )
       .then((response) => {
         const payload = response.data;
         if (payload.data && payload.data.access_token) {
@@ -49,20 +56,26 @@ export default class DreoAPI {
   // Return device list
   public async getDevices(platform, auth) {
     let devices;
-    await axios.get('https://app-api-'+auth.server+'.dreo-cloud.com/api/v2/user-device/device/list', {
-      params: {
-        'pageSize': 1000,
-        'currentPage': 1,
-        'timestamp': Date.now(),
-      },
-      headers: {
-        'authorization': 'Bearer ' + auth.access_token,
-        'ua': 'dreo/2.5.12 (sdk_gphone64_arm64;android 13;Scale/2.625)',
-        'lang': 'en',
-        'accept-encoding': 'gzip',
-        'user-agent': 'okhttp/4.9.1',
-      },
-    })
+    await axios
+      .get(
+        'https://app-api-' +
+          auth.server +
+          '.dreo-cloud.com/api/v2/user-device/device/list',
+        {
+          params: {
+            pageSize: 1000,
+            currentPage: 1,
+            timestamp: Date.now(),
+          },
+          headers: {
+            authorization: 'Bearer ' + auth.access_token,
+            ua: ua,
+            lang: 'en',
+            'accept-encoding': 'gzip',
+            'user-agent': 'okhttp/4.9.1',
+          },
+        },
+      )
       // Catch and log errors
       .then((response) => {
         devices = response.data.data.list;
@@ -77,19 +90,25 @@ export default class DreoAPI {
   // used to initialize power state, speed values on boot
   public async getState(platform, sn, auth) {
     let state;
-    await axios.get('https://app-api-'+auth.server+'.dreo-cloud.com/api/user-device/device/state', {
-      params: {
-        'deviceSn': sn,
-        'timestamp': Date.now(),
-      },
-      headers: {
-        'authorization': 'Bearer ' + auth.access_token,
-        'ua': 'dreo/2.5.12 (sdk_gphone64_arm64;android 13;Scale/2.625)',
-        'lang': 'en',
-        'accept-encoding': 'gzip',
-        'user-agent': 'okhttp/4.9.1',
-      },
-    })
+    await axios
+      .get(
+        'https://app-api-' +
+          auth.server +
+          '.dreo-cloud.com/api/user-device/device/state',
+        {
+          params: {
+            deviceSn: sn,
+            timestamp: Date.now(),
+          },
+          headers: {
+            authorization: 'Bearer ' + auth.access_token,
+            ua: ua,
+            lang: 'en',
+            'accept-encoding': 'gzip',
+            'user-agent': 'okhttp/4.9.1',
+          },
+        },
+      )
       .then((response) => {
         state = response.data.data.mixed;
       })
@@ -104,14 +123,17 @@ export default class DreoAPI {
   // websocket is also used to monitor incoming state changes from hardware controls
   public async startWebSocket(platform, auth) {
     // open websocket
-    const url = 'wss://wsb-'+auth.server+'.dreo-cloud.com/websocket?accessToken='+auth.access_token+'&timestamp='+Date.now();
+    const url =
+      'wss://wsb-' +
+      auth.server +
+      '.dreo-cloud.com/websocket?accessToken=' +
+      auth.access_token +
+      '&timestamp=' +
+      Date.now();
     platform.log.debug(url);
-    const ws = new ReconnectingWebSocket(
-      url,
-      [],
-      {WebSocket: WebSocket});
+    const ws = new ReconnectingWebSocket(url, [], { WebSocket: WebSocket });
 
-    ws.addEventListener('error', error => {
+    ws.addEventListener('error', (error) => {
       platform.log.debug('WebSocket', error);
     });
 
